289daebddeb5ed69821b933ed4073558401fb717
Melissa Real <melissa.real@hotmail.com>
2019-03-22 18:18:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // Code your solution in this file
 
 function findMatching(collection, name){
-  filter(collection, function(n) {
-    if (n === name){
-    return n;
-    });
- }
+  collection.filter(name === name);
 }
 
 function fuzzyMatch(){
