1619107117ca28886564c44f7991f223af211629
Melissa Real <melissa.real@hotmail.com>
2019-03-22 18:17:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+
+function findMatching(collection, name){
+  filter(collection, function(n) {
+    if (n === name){
+    return n;
+    });
+ }
+}
+
+function fuzzyMatch(){
+
+}
+
+function matchName(){
+
+}
