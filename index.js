573ba4c1adaf49d5e0b8c0504768598f9e7d76a0
Melissa Real <melissa.real@hotmail.com>
2019-03-22 18:20:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 
 function findMatching(collection, name){
-  const result = collection.filter(n => n===name);
+  const result = collection.filter(n => n===name.toLowerCase());
   return result;
 }
 
